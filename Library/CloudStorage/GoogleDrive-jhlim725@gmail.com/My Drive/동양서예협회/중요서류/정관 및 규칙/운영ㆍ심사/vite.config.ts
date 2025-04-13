e7d1ce152f2204f88615b040d5f01c4ca2aee475
Jaehong <jhlim725@gmail.com>
2025-04-13 23:45:04 +0900

    Add built files to docs folder

---
+++ Library/CloudStorage/GoogleDrive-jhlim725@gmail.com/My Drive/동양서예협회/중요서류/정관 및 규칙/운영ㆍ심사/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+export default defineConfig(({ mode }) => ({
+  base: '/',
+  build: {
+    outDir: 'docs',
+  },
+  server: {
+    host: "::",
+    port: 8082,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
