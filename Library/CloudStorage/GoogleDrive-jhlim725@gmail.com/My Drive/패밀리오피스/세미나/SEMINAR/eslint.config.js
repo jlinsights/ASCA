b17ca2d1c961572d81ae85c800eb42d5fc797f69
Jaehong <jhlim725@gmail.com>
2025-04-29 13:17:20 +0900

    Update seminar schedule information

---
+++ Library/CloudStorage/GoogleDrive-jhlim725@gmail.com/My Drive/패밀리오피스/세미나/SEMINAR/eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.Config} */
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
+]; 
